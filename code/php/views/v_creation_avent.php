2b366d3a883e886c76ddef01323a407f4bc0ad09
Vince <vmartin@et.esiea.fr>
2024-05-14 10:03:46 +0200

    page de création

--- code/php/views/v_creation_avent.php
+++ code/php/views/v_creation_avent.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="v_creation_avent.css">
     <title>Création AVent</title>
 </head>
 <body>
