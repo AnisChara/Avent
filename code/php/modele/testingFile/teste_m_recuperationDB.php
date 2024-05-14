b08e0fbf76bc20e37ad40086863c3c9ab9f820f2
Axel <gasser@et.esiea.fr>
2024-05-14 16:30:40 +0200

    ajout du teste pour teste_m_recuperationDB

---
+++ code/php/modele/testingFile/teste_m_recuperationDB.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../m_recuperationDB.php';
+
+$pdo = new PDO("mysql:host=localhost;dbname=DB_AVent", "root");     // Le pdo soit le "hook" de connexion.
+
+$query = "SELECT theme_name FROM theme WHERE theme_id = :id";       // Ma query (requête SQL) avec les ":" pour les conditions coté injection
+$id = 2;                    
+$argument = [[":id",$id]];                                          //les arguments  : [0] -> ou remplacer dans la requête  [1] -> ce qu'il faut mettre à la place
+
+echo var_dump(TakeFromDB($pdo,$query,$argument));                   //affiche en var_dump le résultat
+
+?>
+
