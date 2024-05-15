23f3e1945013cc61027721daeb6e9e358e337200
ABDEL-AZIZ <ahmatmahammad@et.intechinfo.fr>
2024-05-15 12:11:09 +0200

    creation module tache

--- code/php/modele/m_taches.php
+++ code/php/modele/m_taches.php
@@ -1,25 +1,26 @@
 <?php
-
 function recuperer_toutes_les_taches() {
-    require "m_connexion_bdd.php";
+    require "m_recuperationDB.php";
     $requete = $db->prepare("SELECT * FROM task;");
     $requete->execute();
     $resultat = $requete->fetchAll();
     return $resultat;
 }
 
-function ajouter_tache($description) {
-    require "m_connexion_bdd.php";
-    $requete = $db->prepare("INSERT INTO task(description) VALUES (?);");
-    $requete->execute(array($description));
-    $resultat = $requete->fetch();
-    return $resultat;
+function ajouter_tache($content, $avent_id) {
+    require "m_recuperationDB.php";
+    $requete = $db->prepare("INSERT INTO task(content,avent_id) VALUES (?,?)");
+    $requete->execute(array($content, $avent_id));
 }
 
-function supprimer_tache($id) {
-    require "m_connexion_bdd.php";
-    $requete = $db->prepare("DELETE FROM task WHERE id = ?");
-    $requete->execute(array($id));
-    $resultat = $requete->fetch();
-    return $resultat;
+function fin_tache($task_id, $finisseur) {
+    require "m_recuperationDB.php";
+    $requete = $db->prepare("UPDATE task SET finisseur =? WHERE task_id =? AND finisseur IS NULL");
+    $requete->execute(array($finisseur, $task_id));
+}
+
+function supprimer_tache($task_id) {
+    require "m_recuperationDB.php";
+    $requete = $db->prepare("DELETE FROM task WHERE task_id =?");
+    $requete->execute(array($task_id));
 }
