c77c4efd069b911c0eec913307abd0a01d9b7f80
Axel <gasser@et.esiea.fr>
2024-05-16 11:40:56 +0200

    change require_once -> require

--- code/php/modele/m_collect_theme_fav_user.php
+++ code/php/modele/m_collect_theme_fav_user.php
@@ -3,7 +3,7 @@
     function getThemeUser($userID)
     {
         require_once 'm_recuperationDB.php';
-        require_once 'm_connexion_bdd.php';
+        require 'm_connexion_bdd.php';
 
         $query = 'SELECT theme.theme_name FROM theme INNER JOIN fav_theme_for_user ON theme.theme_id = fav_theme_for_user.theme_id WHERE fav_theme_for_user.user_id = :user_id';
         $arguments = [[":user_id",$userID]];
