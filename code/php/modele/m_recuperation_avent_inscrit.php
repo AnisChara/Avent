9c8a3aa20eec6a2ef79f380fda490fbaf3898eb4
Chara Anis <chara@et.esiea.fr>
2024-05-22 15:06:06 +0200

    dm

--- code/php/modele/m_recuperation_avent_inscrit.php
+++ code/php/modele/m_recuperation_avent_inscrit.php
@@ -1,7 +1,7 @@
 <?php
 function recuperer_avent_inscrit_user($user_id) {
     require "m_recuperationDB.php";
-    $requete = $db->prepare("SELECT * FROM  inscrit WHERE user_id = ?");
+    $requete = $db->prepare("SELECT avent_id FROM  inscrit WHERE user_id = ?");
     $requete->execute(array($user_id));
     $resultat = $requete;
     return $resultat;
--- code/php/modele/m_recuperation_info_user.php
+++ code/php/modele/m_recuperation_info_user.php
@@ -1,8 +1,8 @@
 <?php
-function recuperer_info_user() {
+function recuperer_info_user($user_id) {
     require "m_recuperationDB.php";
-    $requete = $db->prepare("SELECT * FROM user");
-    $requete->execute();
+    $requete = $db->prepare("SELECT * FROM user WHERE user_id = ?");
+    $requete->execute($user_id);
     $resultat = $requete;
     return $resultat;
 }
