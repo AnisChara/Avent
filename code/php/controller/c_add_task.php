8a8b12f2240f4138a7a19020484ad2cbd3336450
ABDEL-AZIZ <ahmatmahammad@et.intechinfo.fr>
2024-05-27 11:21:18 +0200

    create view modif compte + css

--- code/php/controller/c_add_task.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "../models/m_tache.php"; 
-if (isset($_POST["description"]) && trim($_POST["description"]) != "") {
-    ajouter_tache(trim($_POST["description"]),$avent_id);
-} else {
-    $erreur = "Veuillez remplir la description";
-}
