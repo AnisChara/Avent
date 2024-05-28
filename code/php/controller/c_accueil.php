8bf974439f31da49322305b3a1a5b602dc7c10eb
Chara Anis <chara@et.esiea.fr>
2024-05-28 10:03:25 +0200

    dm

--- code/php/controller/c_accueil.php
+++ code/php/controller/c_accueil.php
@@ -4,4 +4,4 @@ require_once '../modele/m_verif_cookies.php';
 
 verif_cookies(); //si pas de cookies redirection page de co
 
-require './c_afficher_TL.php'; // cookies bien présent
+require './c_afficher_TL.php'; // cookies bien présent ou connexion réussie
