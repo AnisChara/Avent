f556aafa24c130e98976f451c91209ac8085f26f
Chara Anis <chara@et.esiea.fr>
2024-05-28 11:33:51 +0200

    ajt de etat avent

--- code/DATABASE/DB_AVent_creation.sql
+++ code/DATABASE/DB_AVent_creation.sql
@@ -75,6 +75,7 @@ CREATE TABLE avent
     lien_billeterie VARCHAR(255),
     lieu VARCHAR(255),
     capacity INT,
+    etat BOOLEAN
     is_payant BOOLEAN,
     is_public BOOLEAN,
     date_creation TIMESTAMP,
