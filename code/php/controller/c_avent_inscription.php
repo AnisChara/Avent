390188cf864ceabcbb6bf45668d2db8164e05748
Axel <gasser@et.esiea.fr>
2024-06-03 13:54:49 +0200

    dernière partie de MyAvent Les avents où l'utilisateur est inscrit.

--- code/php/controller/c_avent_inscription.php
+++ code/php/controller/c_avent_inscription.php
@@ -1,2 +1,15 @@
 <?php 
-require '../views/v_avent_inscription.php';
+
+require '../modele/m_verif_cookies.php';
+
+verif_cookies();
+
+require '../modele/m_getID_from_mail.php';
+require '../modele/m_recuperation_avent_inscrit.php';
+
+$list_avent_inscrit = recuperer_avent_inscrit_user(get_userID($_COOKIE['email']));
+
+require '../modele/m_collect_nom_user_from_id.php';
+require '../modele/m_collect_img_avent.php';
+
+require 'c_afficher_page_avent_inscription.php';
---
+++ code/php/modele/testingFile/teste_m_recuperation_avent_inscrit.php
@@ -0,0 +1,5 @@
+<?php
+
+require '../m_recuperation_avent_inscrit.php';
+$id = 1;
+var_dump(recuperer_avent_inscrit_user($id));
