5c04109bdf59dd5b132211d7580f1ed783596465
Vince <vmartin@et.esiea.fr>
2024-06-13 10:58:25 +0200

    views creation avent

--- code/php/views/v_creation.php
+++ code/php/views/v_creation.php
@@ -17,47 +17,69 @@
         </ul>
     </div>
     <!-- partie de la création -->
-    <section class="container_colon">
-        <div class="first_colon">
-            <form action="" method="GET">
-                <input type="text" name="nom" value="Mon super Avent">
+    <section class="result">
+        <div class="result-content">
+            <h3>Créer votre AVent !</h3>
+            <fieldset>
+            <div class="liste-produits">
+                <form class="produit" action="" method="POST">
+                    <div class="message">
+                        <?php 
+                            if (isset($message)) 
+                            {
+                                echo $message;
+                            }
+                        ?>
+                    </div>
+                    <input type="text" name="nom" value="" placeholder="Nom de l'AVent">
+                    <label>Choisissez vos différentes thématiques</label>
                     <select name="theme1">
                         <option></option>
                         <?php foreach ($themelist as $theme) {
                             echo '<option>'.$theme['theme_name'].'</option>';
-                        };
-                        ?>
+                        }; ?>
                     </select>
                     <select name="theme2">
                         <option></option>
                         <?php foreach ($themelist as $theme) {
                             echo '<option>'.$theme['theme_name'].'</option>';
-                        };
-                        ?>
+                        }; ?>
                     </select>
                     <select name="theme3">
                         <option></option>
                         <?php foreach ($themelist as $theme) {
                             echo '<option>'.$theme['theme_name'].'</option>';
-                        };
-                        ?>
+                        }; ?>
                     </select>
-                <label><input type="checkbox" name="interest" value="" /> Évènement publique</label>
-                <label>Titre :</label>
-                <input type="text" name="titre" value="" placeholder="ajouter un titre">
-                <label>Description :</label>
-                <textarea name="information"></textarea>
-                <label>Autre Tag :</label>
-                <textarea name="tag"></textarea>
-                <label>Ajouter une image</label>
-                <input type="file" name="image">
-                <div class="image">
-                    <div class="image-prod">
-                        <img src="../views/images/fond_oasis.jpg" alt="">
+                    <label>date de début de l'évènement</label>
+                    <input type="date">
+                    <label>date de fin de l'évènement</label>
+                    <input type="date">
+                    <label>date de fin de l'inscription</label>
+                    <input type="date">
+                    <label><input type="checkbox" name="interest" value="" /> Évènement publique</label>
+                    <label>Description :</label>
+                    <textarea name="information" cols="30" rows="10"></textarea>
+                    <label>Lien de votre site web (facultatif)</label>
+                    <input type="text" name="lien" value="" placeholder="url site web">
+                    <label><input type="checkbox" name="interest" value="" /> Évènement payant</label>
+                    <label>lien de la billeterie</label>
+                    <input type="text" name="billeterie" value="" placeholder="url billeterie">
+                    <label>Autre Tag :</label>
+                    <textarea name="tag" cols="30" rows="10"></textarea>
+                    <label>Ajouter une image</label>
+                    <input type="file" name="image">
+                    <div class="image">
+                        <div class="image-prod">
+                            <img src="../views/images/fond_oasis.jpg" alt="Affiche d'événement">
+                        </div>
                     </div>
-                </div>
-                <input type="submit" value="Ajouter" name="btn-ajouter">
-            </form>
+                    <div class="ajout">
+                        <input type="submit" value="Ajouter" name="btn-ajouter">
+                    </div>
+                </form>
+            </div>
+            </fieldset>
         </div>
     </section>
     <!-- logo -->
@@ -65,12 +87,6 @@
         <img src="../views/images/AVent.png" alt="menu" class="menu_avent">
     </div>
     <script>
-        const menu_avent = document.querySelector(".menu_avent");
-        const nav_options = document.querySelector(".nav_options");
-
-        menu_avent.addEventListener("click", () => {
-            nav_options.classList.toggle("mobile_menu")
-        })
     </script>
 </body>
 </html>
