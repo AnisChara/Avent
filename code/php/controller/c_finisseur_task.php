54b608409a954bd45e209205c384b132769984d5
ABDEL-AZIZ <ahmatmahammad@et.intechinfo.fr>
2024-06-17 10:20:10 +0200

    create controller finisseur and remove finisseur

--- code/php/controller/c_finisseur_task.php
+++ code/php/controller/c_finisseur_task.php
@@ -4,7 +4,5 @@ if (isset($_POST["task_id"]) == false) {
     require "c_afficher_page_full_avent.php";
     exit;
 } 
-
-supprimer_tache($_POST["task_id"]);
-
+fin_tache($_POST["task_id"],$_POST["user_id"]);
 require "c_afficher_page_full_avent.php";
