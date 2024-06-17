3c372424a01c3c939bb3744d111c9e7945b99656
ABDEL-AZIZ <ahmatmahammad@et.intechinfo.fr>
2024-06-17 09:56:09 +0200

    controller remove task

--- code/php/controller/c_add_task.php
+++ code/php/controller/c_add_task.php
@@ -3,8 +3,8 @@
 require_once "../modele/m_taches.php";
 
 if (isset($_POST["description"]) && trim($_POST["description"]) != "") {
-    // Corrected the function name and added the missing comma between the arguments
     ajouter_tache(trim($_POST["description"]), $_POST["avent_id"]);
 } else {
     $erreur = "Veuillez remplir la description";
 }
+require "c_afficher_page_full_avent.php";
