b8bf9cc7ae529ea11cc78c7d7ecc649c96acce7b
Vince <vmartin@et.esiea.fr>
2024-06-18 11:23:38 +0200

    modification demandé par Anis

--- code/php/views/v_creation.php
+++ code/php/views/v_creation.php
@@ -20,9 +20,9 @@
     <section class="result">
         <div class="result-content">
             <h3>Créer votre AVent !</h3>
-            <fieldset>
             <div class="liste-produits">
                 <form class="produit" action="" method="POST">
+                    <!-- message d'erreur -->
                     <div class="message">
                         <?php 
                             if (isset($message)) 
@@ -31,8 +31,10 @@
                             }
                         ?>
                     </div>
+                    <!-- titre de l'évènement -->
                     <input type="text" name="nom" value="" placeholder="Nom de l'AVent">
                     <label>Choisissez vos différentes thématiques</label>
+                    <!-- Sélection des thèmes -->
                     <select name="theme1">
                         <option></option>
                         <?php foreach ($themelist as $theme) {
@@ -51,22 +53,29 @@
                             echo '<option>'.$theme['theme_name'].'</option>';
                         }; ?>
                     </select>
+                    <!-- les trois dates (fin, debut, inscription) -->
                     <label>date de début de l'évènement</label>
                     <input type="date">
                     <label>date de fin de l'évènement</label>
                     <input type="date">
                     <label>date de fin de l'inscription</label>
                     <input type="date">
+                    <!-- évènement publique -->
                     <label><input type="checkbox" name="interest" value="" /> Évènement publique</label>
+                    <!-- description -->
                     <label>Description :</label>
                     <textarea name="information" cols="30" rows="10"></textarea>
+                    <!-- lien d'un potentiel site web -->
                     <label>Lien de votre site web (facultatif)</label>
                     <input type="text" name="lien" value="" placeholder="url site web">
-                    <label><input type="checkbox" name="interest" value="" /> Évènement payant</label>
+                    <!-- évènement payant + lien billeterie -->
+                    <label><input type="checkbox" id="price" name="interest" value="" /> Évènement payant</label>
                     <label>lien de la billeterie</label>
-                    <input type="text" name="billeterie" value="" placeholder="url billeterie">
+                    <input type="text" name="billeterie" id="link" value="" placeholder="url billeterie">
+                    <!-- autre tag que l'utilisateur souhaite mettre -->
                     <label>Autre Tag :</label>
                     <textarea name="tag" cols="30" rows="10"></textarea>
+                    <!-- Ajout d'une image -->
                     <label>Ajouter une image</label>
                     <input type="file" name="image">
                     <div class="image">
@@ -74,12 +83,12 @@
                             <img src="../views/images/fond_oasis.jpg" alt="Affiche d'événement">
                         </div>
                     </div>
+                    <!-- ajout de l'évènement -->
                     <div class="ajout">
                         <input type="submit" value="Ajouter" name="btn-ajouter">
                     </div>
                 </form>
             </div>
-            </fieldset>
         </div>
     </section>
     <!-- logo -->
