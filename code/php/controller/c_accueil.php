a6d751523f305987dc6b2d2ab185bcdc28df231e
Axel <gasser@et.esiea.fr>
2024-06-18 13:43:35 +0200

    modif page suggestion

--- code/php/controller/c_accueil.php
+++ code/php/controller/c_accueil.php
@@ -9,11 +9,13 @@ require_once '../modele/m_get_suggestion.php';
 require_once '../modele/m_collect_img_avent.php';
 require_once '../modele/m_gen_suggestion.php';
 require_once '../modele/m_collect_avent_is_like.php';
+require_once '../modele/m_collect_theme_for_avent.php';
 
 $AventDisplay = get_suggestion(get_userID($_COOKIE['email']))[$_COOKIE['suggestion']];
 if(isset($AventDisplay))
 {
     $like = islike(get_userID($_COOKIE["email"]),$AventDisplay['avent_id']);
+    $themelist = collect_theme_for_avent($AventDisplay['avent_id']);
 }
 //var_dump($AventDisplay);
 require './c_afficher_TL.php'; // cookies bien présent ou connexion réussie
