70f26ba609bb8b7d4ab08601f47eee3e5b9a5d7a
Chara Anis <chara@et.esiea.fr>
2024-06-19 11:20:35 +0200

    la c bon

--- code/php/controller/c_deconnexion.php
+++ code/php/controller/c_deconnexion.php
@@ -1,8 +1,6 @@
 <?php
 
-require 'c_verif_cookies.php';
-
 setcookie('email', "", time()-3600);
 setcookie('mot_de_passe', "", time()-3600);
 
-verif_cookies();
+require 'c_afficher_page_de_co.php';
