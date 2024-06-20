62dd57f9209e0fdf098f1250a73b6c45db489c9f
Chara Anis <chara@et.esiea.fr>
2024-06-20 10:07:02 +0200

    design connexion

--- code/php/views/v_connexion.php
+++ code/php/views/v_connexion.php
@@ -2,39 +2,23 @@
 <html>
 	<head>
 		<title>Accueil</title>
+		<link href='https://fonts.googleapis.com/css?family=League Spartan' rel='stylesheet'>
 		<link rel="stylesheet" type="text/css" href="../views/css/v_connexion.css">
 		<meta charset="utf-8">
 	</head>
-<body>	
-	<h1>Avent, créez, partagez, profitez !</h1>
-		<div class="login-box">
-			<form action="./c_connexion.php" method="POST">
-				<div class ="titre">
-					<h2 class="light">connexion</h2>
-					<a href="c_afficher_page_inscription.php"><h2 class="underline">s'incrire</h2></a>
-				</div>
-				<?php if ( isset($error_connexion_msg)) echo $error_connexion_msg; ?>
-				<div class="icon">
-					<img src="../views/images/user.svg" alt=""/>
-				</div>
-				<div class = "box">
-					<input type="text" name="email" value="">
-					<label>Email</label>
-				</div>
-				<div class = "box">
-					<input type="password" name="mot_de_passe" value="">
-					<label>Mot de passe</label>
-				</div>
-				<div class = "connect">
-					<input type="submit" name="action" value="Se connecter">
-				</div>
-				<div class="register">
-					<p>Avent, créez, partagez, profitez !</p>
-				</div>
-			</form>
-		</div>
-	<div class="logo">
-		<img src="../views/images/AVent.png" alt="">
+<body>
+	<div class="logo-container">
+	<img src="../views/images/logo_avent.png" class="logo">
 	</div>
+	
+	<div class="slogan">
+		<h2>Créez, partagez, profitez !</h2>
+	</div>
+	
+	<form action="c_connexion.php" method="POST" class="form">
+		<input type="text" name="email" class="box" placeholder="adresse mail">
+		<input type="password" name="mot_de_passe" class="box" placeholder="mot de passe">
+		<input type="submit" name="action" value="Se connecter">
+	</form>
 </body>
 </html>
