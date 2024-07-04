01d63f2fc57d0fe46b5520fbcd6359ab0106788a
Vince <vmartin@et.esiea.fr>
2024-07-04 09:47:53 +0200

    changement dans la views compte

--- code/php/views/v_page_compte.php
+++ code/php/views/v_page_compte.php
@@ -20,52 +20,52 @@
     <div class="floating-btn">
         <img src="../views/images/AVent.png" alt="Floating Menu" class="menu_avent">
     </div>
-    <div class="container">
-        <div class="container_content">
-            <div class="container_left">
-                <form action="c_modif_compte.php" method="POST">
+    <form action="c_modif_compte.php" method="POST">
+        <div class="container">
+            <div class="container_content">
+                <div class="container_left">
                     <div class="user">
                         <img src="../views/images/user.svg" alt="">
                     </div>
                     <div class="box_start">
                         <label>pseudo</label>
                         <input type="text" name="pseudo" value="<?php echo $info['pseudo'] ?>">
-                        
-                        <label>thèmes favoris</label>
-                        <?php
-                            foreach ($themelist as $theme)
-                            {
-                                echo '<button class="nav_button" disabled>'.$theme.'</button>';
-                            }    
-                        ?> 
+                                
+                                <label>thèmes favoris</label>
+                                <?php
+                                    foreach ($themelist as $theme)
+                                    {
+                                        echo '<button class="nav_button" disabled>'.$theme.'</button>';
+                                    }    
+                                ?> 
+                            </div>
                     </div>
-                </form>
-            </div>
-            <div class="container_right">
-				<h2>Votre Profil</h2>
-					<div class="box">
-                    	<div class="box_one">
-                        	<!-- <?php if ( isset($error_modif_msg)) echo $error_modif_msg; ?>-->
-                        	<label>nom</label>
-                        	<input type="text" name="nom" value=<?php echo $info['nom'] ?>>
-                        	<label>prenom</label>
-                        	<input type="text" name="prenom" value="<?php echo $info['prenom'] ?>">
-                        	<label>Date de Naissance</label>
-                        	<input type="text" name="date_de_naissance" placeholder="jj/mm/aaaa" value="<?php echo $info['date_naissance']?>">
-                    	</div>
-                    	<div class="box_two">
-                        	<label>email</label>
-                        	<input type="text" name="email" value="<?php echo $info['email']?>">
-                        	<label>Mot de passe</label>
-                        	<input type="password" name="mot_de_passe" value="<?php echo $info['mot_de_passe']?>">
-                    	</div>
-                    	<div class="sauvegarder">
-							<input type="submit" value="Sauvegarder">
-                    	</div>
-					</div>
+                    <div class="container_right">
+                        <h2>Votre Profil</h2>
+                        <div class="box">
+                            <div class="box_one">
+                                <!-- <?php if ( isset($error_modif_msg)) echo $error_modif_msg; ?>-->
+                                <label>nom</label>
+                                <input type="text" name="nom" value=<?php echo $info['nom'] ?>>
+                                <label>prenom</label>
+                                <input type="text" name="prenom" value="<?php echo $info['prenom'] ?>">
+                                <label>Date de Naissance</label>
+                                <input type="text" name="date_de_naissance" placeholder="jj/mm/aaaa" value="<?php echo $info['date_naissance']?>">
+                            </div>
+                            <div class="box_two">
+                                <label>email</label>
+                                <input type="text" name="email" value="<?php echo $info['email']?>">
+                                <label>Mot de passe</label>
+                                <input type="password" name="mot_de_passe" value="<?php echo $info['mot_de_passe']?>">
+                            </div>
+                            <div class="sauvegarder">
+                                <input type="submit" value="Sauvegarder">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </div>  
         </div>
-    </div>  
+    </form>
 </body>
 </html>
